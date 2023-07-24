50cda75fe011867e62e4bc0ef57259369f61489e
JoshSwain <joshfswain@gmail.com>
2023-07-23 20:12:09 -0400

    Pickup later

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,17 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+
+const changeEvent = () => {
+    
+}
+
 const Budget = () => {
     const { budget } = useContext(AppContext);
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            {/*<span>Budget: £{budget}</span>*/}
+            {/*<span><input type="value"  onChange={event=> changeEvent()}></input></span>*/}
+
         </div>
     );
 };
